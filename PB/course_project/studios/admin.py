643b483452f6448ff56560333f28ea9ecbf52447
tupi2001 <nada.geldin@gmail.com>
2022-11-15 23:10:44 -0500

    Studios base code. Not yet tested

---
+++ PB/course_project/studios/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Studio, AmenitySet, Amenity, ImageSet, Image
+
+# Register your models here.
+
+
+class AmenitiesInline(admin.TabularInline):
+    model = Studio.amenities.through
+
+
+class ImagesInline(admin.TabularInline):
+    model = Studios.images.through
+
+
+class StudioAdmin(admin.ModelAdmin):
+    inline = [AmenitiesInline, ImagesInline]
+
+    class Meta:
+        model = Studio
+
+admin.site.register(Studio, StudioAdmin)
+admin.site.register(AmenitySet)
+admin.site.register(Amenity)
+admin.site.register(ImageSet)
+admin.site.register(Image)
