d8d8c9f439dab293b3ecfa9b8a61ae741121d488
tupi2001 <nada.geldin@gmail.com>
2022-11-19 18:37:43 -0500

    Removing extra files

--- PB/course_project/course_project/settings.py
+++ PB/course_project/course_project/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'studios',
-    'subscriptions',
     'rest_framework.authtoken',
 ]
 
--- PB/course_project/course_project/urls.py
+++ PB/course_project/course_project/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('subscriptions/', include('subscriptions.urls', namespace='subscriptions')),
     path("api-auth/", include("rest_framework.urls")),
 ]
