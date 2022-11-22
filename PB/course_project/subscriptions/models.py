0c3511dc3fd8f064e96b9e9f5327e69f766797ee
oliverlaranjeira <oliver.laranjeira@mail.utoronto.ca>
2022-11-22 03:02:17 -0400

    documented models

--- PB/course_project/subscriptions/models.py
+++ PB/course_project/subscriptions/models.py
@@ -12,39 +12,61 @@ DURATION = (
 )
 
 class Subscriptions(models.Model):
+    """Subscriptions models: model for subscriptions
+        Parameters:
+            value: value of subscription
+            charge_every: how often to charge user
+    """
     value = models.FloatField(null=False)
 
     charge_every = models.CharField(choices=DURATION, max_length=5, default='Month')
 
     def __str__(self):
+        """function that returns model as a string"""
         return str(self.value) + ' per ' + str(self.charge_every)
 
-    # def create_subscription(self, value, charge_every):
-    #    if not value or not charge_every:
-    #       raise ValueError('This is a required field')
-    #   
-    #    subscription = self.model(value=value, charge_every=charge_every)
-    #    subscription.save()
-    #    return subscription
-
 class Card(models.Model):
-    user = models.ForeignKey(to=CustomUser, null=False, on_delete=models.CASCADE)
+    """Card linked to a specific user
+        Parameters:
+            user: user linked to this card
+            name: name on card
+            card: card number (including expiration date and cvv)
+    """
+    user = models.OneToOneField(to=CustomUser, null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=False)
     card = models.CharField(max_length=23, validators=[MinLengthValidator(23)], null=False)
 
     def __str__(self):
+        """function that returns model as a string"""
         return str(self.card)
 
 class Payment(models.Model):
+    """Individual payments from a given individual for a given subscripion
+        Parameters:
+            user: user linked to this payment
+            subscription: type of subscription
+            date: when payment was made
+            card: which card instance was used
+    """
     user = models.ForeignKey(to=CustomUser, null=False, on_delete=models.CASCADE)
     subscription = models.ForeignKey(to=Subscriptions, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-    card = models.OneToOneField(Card, on_delete=models.CASCADE, null=False)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
+        """function that returns model as a string"""
         return str(self.subscription.value) + ', paid on ' + str(self.date)
 
 class UserSub(models.Model):
+    """Model that links user to a subscription
+        Parameters:
+            user: custom_user object
+            subscription: type of subscription currently assigned to user
+            card: card linked to user, which will be charged
+            active: if the subscription is currently active, ongoin
+            renew: if the card should be charged again next billing cycle
+            renew_date: when the card should be charged once again
+    """
     user = models.OneToOneField(to=CustomUser, null=False, on_delete=models.CASCADE, unique=True)
     subscription = models.ForeignKey(Subscriptions, blank=True, on_delete=models.CASCADE)
     card = models.OneToOneField(Card, blank=True, on_delete=models.CASCADE)
@@ -53,15 +75,17 @@ class UserSub(models.Model):
     renew_date = models.DateTimeField(auto_now_add=True)
 
     def renew_required(self):
+        """function that checks if card needs to be charged"""
 
         if date.today >= self.renew_date:
             if self.renew:
-                if self.subscription.charge_every == 'mon':
-                    self.renew_date = date.today + relativedelta(months=+1)
-                if self.subscription.charge_every == 'yea':
-                    self.renew_date = date.today + relativedelta(years=1)
-                
                 # charge card in theory
+                Payment.objects.create(user=self.user, subscription=self.subscription, date=date.today, card=self.card)
+
+                if self.subscription.charge_every == 'Month':
+                    self.renew_date = date.today + relativedelta(months=+1)
+                else:
+                    self.renew_date = date.today + relativedelta(years=1)
 
                 self.save()
 
@@ -70,12 +94,7 @@ class UserSub(models.Model):
             self.active = False
             return False
 
-    def cancel_future(self):
-        self.renew = False
-
-    def change_subscription(self, subscription):
-        self.subscription = subscription
-
     def __str__(self):
+        """function that returns model as a string"""
         return str(self.user.__str__) + ' ' + str(self.subscription.__str__)
 
