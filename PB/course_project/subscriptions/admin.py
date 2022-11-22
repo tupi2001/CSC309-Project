75252329aa496bd17907dd794e02b4ec8eb62461
oliverlaranjeira <oliver.laranjeira@mail.utoronto.ca>
2022-11-22 03:01:54 -0400

    documentation

--- PB/course_project/subscriptions/admin.py
+++ PB/course_project/subscriptions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from subscriptions.models import Subscriptions, UserSub, Card, Payment
 
-# Register your models here.
+# admin can create/edit subscription types, can register/edit a subscription to a given user, 
+# can add/edit user cards and add/edit user payments
 
 admin.site.register(Subscriptions)
 admin.site.register(UserSub)
