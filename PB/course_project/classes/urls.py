6e976161d3e237d5ab81ec2bea014c0ca7766289
alissalozhkin <65978304+alissalozhkin@users.noreply.github.com>
2022-11-22 21:52:24 -0500

    Update urls.py
    Signed-off-by: alissalozhkin <65978304+alissalozhkin@users.noreply.github.com>

--- PB/course_project/classes/urls.py
+++ PB/course_project/classes/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from classes.views import CreateClassView, UpdateClassView, DeleteClassView, ClassesView
+from classes.views import CreateClassView, UpdateClassView, DeleteClassView, ClassesView, UserClassesView
+from classes.views import EnrolUserInClassView, RemoveUserFromClassesView, RemoveUserFromClassView
+from classes.views import DeleteClassesView
 
 app_name = 'classes'
 
 urlpatterns = [
-    path('create-class/', CreateClassView.as_view(), name='create'),
-    path('update-class/', UpdateClassView.as_view(), name='update'),
-    path('delete-class/', DeleteClassView.as_view(), name='delete_class'),
-    path('view-classes', ClassesView.as_view(), name='view-classes')
+    path('create/', CreateClassView.as_view(), name='create'),
+    path('<int:class_id>/update/', UpdateClassView.as_view(), name='update'),
+    path('<int:studio_id>/delete/<int:class_id>/', DeleteClassView.as_view(), name='delete'),
+    path('<int:studio_id>/delete/<int:class_id>/all/', DeleteClassesView.as_view(), name='delete'),
+    path('<int:studio_id>/view/', ClassesView.as_view(), name='view'),
+    path('user/<int:studio_id>/enrol/<int:class_id>/', EnrolUserInClassView.as_view(), name='view'),
+    path('user/<int:studio_id>/enrol/<int:class_id>/all/', EnrolUserInClassView.as_view(), name='view'),
+    path('user/<int:studio_id>/unenrol/<int:class_id>/', RemoveUserFromClassView.as_view(), name='view'),
+    path('user/<int:studio_id>/unenrol/<int:class_id>/all/', RemoveUserFromClassesView.as_view(), name='view'),
+    path('user/view/', UserClassesView.as_view(), name='view'),
 ]
