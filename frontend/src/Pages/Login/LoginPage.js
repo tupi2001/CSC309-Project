9f30637c131dd203aa2fbfc2e3f1b9e1cae68cd3
tupi2001 <nada.geldin@gmail.com>
2022-12-05 21:58:48 -0500

    check

---
+++ frontend/src/Pages/Login/LoginPage.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import styles from "./LoginPage.module.css";
+
+const LoginPage = () => {
+  const dosomething = (event) => {
+    event.preventDefault();
+    console.log("hello");
+  };
+  return (
+    <form className={styles.form} onSubmit={dosomething}>
+      <div className={styles["form-content"]}>
+        <h1 className={styles["form-title"]}>Please sign in</h1>
+        <input
+          type="email"
+          placeholder="Email address"
+          autoFocus
+          className={styles["form-input"]}
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          className={styles["form-input"]}
+        ></input>
+        <Link to="/signup/" className={styles["form-redirect"]}>
+          Don't have an account?
+        </Link>
+        <button className={styles["form-btn"]}>Sign in</button>
+        <p className={styles["copyright-tag"]}>
+          Copyright MKA Group &copy; 2022
+        </p>
+      </div>
+    </form>
+  );
+};
+
+export default LoginPage;
